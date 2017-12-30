c79a949547020e3c93d4234207c4c056dbfa5ec7
Salma Mohammed <sm.71175@gmail.com>
2017-12-30 22:10:15 +0200

    Refactor Responsiveness

--- js/main.js
+++ js/main.js
@@ -139,12 +139,12 @@ createRestaurantHTML = (restaurant) => {
   const li = document.createElement('li');
 
   const image = document.createElement('img');
-  image.setAttribute('alt', restaurant.name + ' restaurant image');
+  image.setAttribute('alt', restaurant.name + ' restaurant');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('h2');
   name.innerHTML = restaurant.name;
   li.append(name);
 
