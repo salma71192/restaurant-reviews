bcb0ecc4b22d46c38bcb61bc5b85da3de7eacfbb
Salma Mohammed <sm.71175@gmail.com>
2018-04-23 12:23:16 +0200

    Create restaurants objectStore.

--- js/indexDB.js
+++ js/indexDB.js
@@ -8,5 +8,22 @@ var idbApp = (function() {
     return;
   }
 
+  var dbPromise = idb.open('couches-n-restaurants', 1, function(upgradeDb) {
+    switch (upgradeDb.oldVersion) {
+      case 0:
+        // a placeholder case so that the switch block will
+        // execute when the database is first created
+        // (oldVersion is 0)
+      case 1:
+        console.log('Creating the restaurants object store');
+        upgradeDb.createObjectStore('restaurants', {keyPath: 'id'});
+
+      // TODO 4.1 - create 'name' index
+      case 2:
+        console.log('Creating a name index');
+        var store = upgradeDb.transaction.objectStore('restaurants');
+        store.createIndex('name', 'name', {unique: true});
+    }
+  });
 
 })();
