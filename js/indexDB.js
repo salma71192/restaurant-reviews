cff9c6677b45bc117cc5b29d04eb99fee31d33ba
Salma Mohammed <sm.71175@gmail.com>
2018-04-23 12:22:17 +0200

    Check for indexedDB support.

---
+++ js/indexDB.js
@@ -0,0 +1,12 @@
+
+var idbApp = (function() {
+  'use strict';
+
+  // check for support
+  if (!('indexedDB' in window)) {
+    console.log('This browser doesn\'t support IndexedDB');
+    return;
+  }
+
+
+})();
