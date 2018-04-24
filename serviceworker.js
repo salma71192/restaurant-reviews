a0a290e31f35b0d65c8ca2060b70a56460ec087f
Salma Mohammed <sm.71175@gmail.com>
2018-04-24 16:44:08 +0200

    Intercept network requests.

--- serviceworker.js
+++ serviceworker.js
@@ -1,4 +1,6 @@
 var CACHE_NAME = 'my-site-cache-v1';
+
+// cache the application shell
 var urlsToCache = [
   '/',
   '/css/',
@@ -15,5 +17,28 @@ self.addEventListener('install', function(event) {
         console.log('Opened cache');
         return cache.addAll(urlsToCache);
       })
+  );
+});
+
+// Intercept network requests
+
+self.addEventListener('fetch', function(event) {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+
+      // Add fetched files to the cache
+
+    }).catch(function(error) {
+
+      // Respond with custom offline page
+
+    })
   );
 });
