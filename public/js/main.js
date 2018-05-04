8f58ee11f37b9b757a10c72cf60b13769a444235
Salma Mohammed <sm.71175@gmail.com>
2018-05-04 20:38:42 +0200

    Refactor: use indexedDB from service worker.

--- public/js/main.js
+++ public/js/main.js
@@ -196,8 +196,3 @@ if ('serviceWorker' in navigator) {
     });
   });
 }
-
-// check for support
-if (!('indexedDB' in window)) {
-    console.log('This browser doesn\'t support IndexedDB');
-}
--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,3 +1,6 @@
+var window = self;
+importScripts('./js/idb.js');
+importScripts('./js/dbhelper.js');
 var CACHE_NAME = 'version-12';
 
 // cache the application shell
@@ -43,47 +46,73 @@ self.addEventListener('fetch', function(event) {
       });
     })
   );
-});
+})
+
 
 function createDB() {
-  var dbPromise = idb.open('couches-n-restaurants', 5, function(upgradeDb) {
-        switch (upgradeDb.oldVersion) {
-            case 0:
-                // a placeholder case so that the switch block will
-                // execute when the database is first created
-                // (oldVersion is 0)
-            case 1:
-                upgradeDb.createObjectStore('restaurants');
-                // create 'name' index
-            case 2:
-                var store = upgradeDb.transaction.objectStore('restaurants');
-                store.createIndex('name', 'name', {
-                    unique: true
-                });
-        }
-    });
+  'use strict';
 
-    // Store restaurants in IndexedDB
-    function storeRestaurants() {
-        dbPromise.then(function(db) {
-            DBHelper.fetchRestaurants((error, restaurants) => {
-                var tx = db.transaction('restaurants', 'readwrite');
-                var store = tx.objectStore('restaurants');
-                return Promise.all(restaurants.map(function(restaurant) {
-                        return store.add(restaurant);
-                    }))
-                    .then(() => console.log('All restaurants have been added.'));
-            });
-        });
+  // check for support
+  if (!('indexedDB' in window)) {
+    console.log('This browser doesn\'t support IndexedDB');
+    return;
+  }
+
+  var dbPromise = idb.open('couches-n-restaurants', 3, function(upgradeDb) {
+    switch (upgradeDb.oldVersion) {
+      case 0:
+        // a placeholder case so that the switch block will
+        // execute when the database is first created
+        // (oldVersion is 0)
+      case 1:
+        console.log('Creating the restaurants object store');
+        upgradeDb.createObjectStore('restaurants', {keyPath: 'id'});
+
+      // create 'name' index
+      case 2:
+        console.log('Creating a name index');
+        var store = upgradeDb.transaction.objectStore('restaurants');
+        store.createIndex('name', 'name', {unique: true});
     }
-}
+  });
 
+  // Store restaurants in IndexedDB
+  (function storeRestaurants() {
+    dbPromise.then(function(db) {
+      DBHelper.fetchRestaurants((error, restaurants) => {
+        console.log(restaurants);
+
+        var tx = db.transaction('restaurants', 'readwrite');
+        var store = tx.objectStore('restaurants');
+        return Promise.all(restaurants.map(function(restaurant){
+          return store.add(restaurant);
+        }))
+        .then(() => console.log('All restaurants have been added.'));
+      });
+    });
+  })();
+}
 
 // Remove old cache versions
 self.addEventListener('activate', function(event) {
-  var cacheWhitelist = [CACHE_NAME];
-  event.waitUntil(
-    // indexedDB
+
+    console.log('Activating new service worker...');
+
+    var cacheWhitelist = [CACHE_NAME];
+
+    event.waitUntil(
+      /*
+      new Promise(function(resoled, rejected) {
+        caches.keys().then(function(cacheNames) {
+          return Promise.all(
+            cacheNames.map(function(cacheName) {
+              if (cacheWhitelist.indexOf(cacheName) === -1) {
+                return caches.delete(cacheName);
+              }
+            })
+          );
+        })
+      })*/
       createDB()
-  );
+    );
 });
