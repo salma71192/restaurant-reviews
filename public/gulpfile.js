f69d9796a8e221ff267c545f73b0e85b86037688
Salma Mohammed <sm.71175@gmail.com>
2018-05-06 01:51:29 +0200

    Update gulpfile.js and add build directory.

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,8 +1,32 @@
 const gulp = require('gulp');
 const webp = require('gulp-webp');
+const concat = require('gulp-concat');
+const minify = require('gulp-minify');
+const cleanCss = require('gulp-clean-css');
 
-gulp.task('default', () =>
-    gulp.src('img/*.jpg')
-        .pipe(webp())
-        .pipe(gulp.dest('dist/'))
-);
+gulp.task('convert-to-webp', function() {
+  return gulp.src('img/*.jpg')
+      .pipe(webp())
+      .pipe(gulp.dest('build/img/'))
+});
+
+gulp.task('pack-js', function () {
+	return gulp.src(['js/*.js'])
+		.pipe(concat('bundle.js'))
+		.pipe(minify({
+      	ext:{
+      		min:'.js'
+      	},
+      	noSource: true
+      }))
+		.pipe(gulp.dest('build/js'));
+});
+
+gulp.task('pack-css', function () {
+	return gulp.src(['css/*.css'])
+		.pipe(concat('stylesheet.css'))
+		.pipe(cleanCss())
+   .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('default', ['convert-to-webp', 'pack-js', 'pack-css']);
