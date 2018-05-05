dc929a24014beea28b945608169c89b9c4b3a4a7
Salma Mohammed <sm.71175@gmail.com>
2018-05-05 02:11:47 +0200

    Refactor: change images path.

--- public/js/dbhelper.js
+++ public/js/dbhelper.js
@@ -154,7 +154,7 @@ class DBHelper {
    * Restaurant image URL.
    */
   static imageUrlForRestaurant(restaurant) {
-    return (`/img/${restaurant.photograph}.jpg`);
+    return (`/dist/${restaurant.photograph}.webp`);
   }
 
   /**
