a2b536949bfa3dfb877970532e0c0d4ce9a02a27
Salma Mohammed <sm.71175@gmail.com>
2018-05-25 20:56:44 +0200

    Update gulpfile.js , add service worker.

--- public/gulpfile.js
+++ public/gulpfile.js
@@ -1,3 +1,4 @@
+
 const gulp = require('gulp');
 const webp = require('gulp-webp');
 const minify = require('gulp-minify');
@@ -32,4 +33,14 @@ gulp.task('pack-css', function () {
    .pipe(gulp.dest('build/css'));
 });
 
-gulp.task('default', ['convert-jpg-to-webp', 'convert-png-to-webp', 'pack-js', 'pack-css']);
+gulp.task('generate-service-worker', function(callback) {
+  var swPrecache = require('sw-precache');
+  var rootDir = './';
+
+  swPrecache.write(`${rootDir}serviceworker.js`, {
+    staticFileGlobs: [rootDir + '**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+    stripPrefix: rootDir
+  }, callback);
+});
+
+gulp.task('default', ['convert-jpg-to-webp', 'convert-png-to-webp', 'pack-js', 'pack-css', 'generate-service-worker']);
